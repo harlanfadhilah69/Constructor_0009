518fe50f131cee097bb47d758176846f46ec0ad1
harlanfadhilah69 <harlancoegg@gmail.com>
2024-05-14 17:41:50 +0700

    member statik

--- Constructor.cpp
+++ Constructor.cpp
@@ -38,6 +38,13 @@ int main()
 	mahasiswa mhs1("Lia Kurnia");
 	mahasiswa mhs2("Asroni");
 	mahasiswa mhs3("Andi Kurniawan");
-	mahasiswa mhs3("Joko Purbo");
+	mahasiswa mhs4("Joko Purbo");
 
+
+	mhs1.printAll();
+
+	mhs2.printAll();
+	mhs3.printAll();
+	mhs4.printAll();
+	return 0;
 }
